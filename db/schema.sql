f27629c821a88cc671c054507da45ec52d786db2
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-09 18:31:29 -0700

    Schema File working

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,27 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
 USE employee_db;
 
-CREATE TABLE department(
-    id INT AUTO_INCREMENT NOT NULL,
-    department_name VARCHAR(30) NULL,
-    PRIMARY KEY (id)
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
-    id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL NULL,
-    department_id INT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id);
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
-    role_id INT NULL,
-    manager_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    PRIMARY KEY (id)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
